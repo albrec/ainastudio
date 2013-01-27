d2478a5c3b35068b240ef70ddfdac93f0db3d28d
David Souza <David.Souza@bestbuy.com>
2013-01-27 11:53:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ainastudio::Application.config.secret_token = 'c785d09e90b513b6eeee1094bd3dbbae964d4707bde777c58f73345a133d7ddae736009bb727f08f213b4f313b723477d3a9c4e414ec87aa7c1a677048eb36b6'
